2d982cb5945265e06d56ed18c6864a58ef18f2aa
WangChenglin <wangcl.cw@gmail.com>
2017-10-02 04:00:17 -0700

    update max sub-array method.
    Signed-off-by: WangChenglin <wangcl.cw@gmail.com>

---
+++ max_subsequence.cc
@@ -0,0 +1,54 @@
+#ifndef __MAX_SUBARRAY_HPP_
+#define __MAX_SUBARRAY_HPP_
+
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+
+/* i, j indicates the range of subsequence which is continuous.
+ *  return tuple {low-bound, high-bound, max-sum}
+ */
+std::tuple<int, int, int> max_sub_sequence(int *A, int length)
+{
+    int max_sum = 0, sum = 0, low= 0, high = 0;
+    for(int i = 0; i < length; ++i) {
+        for(int j = i; j < length; ++j) {
+            sum = 0;
+            for(int k = i; k <= j; k++) {
+                sum += A[k];
+                if(sum > max_sum) {
+                    max_sum = sum;
+                    low = i;
+                    high = j;
+                }
+            }
+        }
+    }
+
+    return std::make_tuple(low, high, max_sum);
+}
+
+std::tuple<int, int, int> max_sub_sequence2(int *A, int length) {
+    int max_sum = 0, sum = 0, low = 0, high= 0;
+    for(int i = 0; i < length; i++) {
+        sum = 0;
+        for(int j = i; j < length; j++) {
+            sum += A[j];
+            if(sum > max_sum) {
+                max_sum = sum;
+                low = i;
+                high = j;
+            }
+        }
+    }
+
+    return std::make_tuple(low, high, sum);
+}
+
+std::tuple<int, int, int> find_max_crossing_subsequence(int *A, int length) {
+
+}
+
+#endif
