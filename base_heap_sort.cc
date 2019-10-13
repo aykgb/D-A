eff8a963db55e13fe0d2557d0e59799897b3acd4
wangchenglin <wangcl.cw1@gmail.com>
2019-10-13 18:21:23 +0800

    update
    Signed-off-by: wangchenglin <wangcl.cw1@gmail.com>

--- base_heap_sort.cc
+++ base_heap_sort.cc
@@ -1,4 +1,6 @@
+#include <assert.h>
 #include <algorithm>
+#include <chrono>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,25 +16,94 @@ static int parent(int i) { return (i - 1) / 2; }
 
 class Heap {
  public:
-  Heap() : count(0) {}
+  Heap() : count_(0) {}
+  Heap(vector<int>&& data) : data_(data) {  //! 移动构造函数
+    /**
+     * 加入一个二叉树有N(>1)个节点，那么按层遍历，这棵树的最后一个非叶子
+     * 节点是第N/2个，线性结构存储索引为N/2 - 1
+     */
+    count_ = data_.size();
+    for (auto i = count_ / 2 - 1; i > -1; i--) {
+      siftDown(i);
+    }
+  }
 
-  int top() {}
+  bool empty() const { return (0 == count_); }
 
-  void pop() {}
+  int top() { return data_[0]; }
 
-  void push(int i) {}
+  void pop() {
+    data_[0] = data_[--count_];
+    data_.resize(count_);
+    siftDown(0);
+  }
 
-  void clear() {}
+  void push(int item) {
+    auto i = count_++;
+    data_.emplace_back(item);
+    siftUp(i);
+  }
+
+  void clear() {
+    count_ = 0;
+    data_.resize(0);
+  }
 
  protected:
-  void siftUp(int i) {}
+  void siftUp(int i) {
+    assert(i < count_);
+    while (i > 0) {
+      int p = parent(i);
+      if (data_[i] < data_[p]) {
+        std::swap(data_[i], data_[p]);
+        i = p;
+      } else {
+        break;
+      }
+    }
+  }
 
-  void siftDown(int i) {}
+  void siftDown(int i) {
+    // print_result(data_, 0, i, count_);
+    while (i < count_) {
+      auto li = lhs(i);
+      auto ri = rhs(i);
+      auto smallest = i;  //! 求一个最小子树中的最小的节点索引
+      if (li < count_ && data_[smallest] > data_[li]) {
+        smallest = li;
+      }
+      if (ri < count_ && data_[smallest] > data_[ri]) {
+        smallest = ri;
+      }
+      if (smallest != i) {
+        std::swap(data_[i], data_[smallest]);
+      } else {
+        break;
+      }
+      i = smallest;
+    }
+  }
+
+  void take(vector<int>& data) {
+    data = std::move(data_);
+    count_ = 0;
+    data.resize(0);
+  }
 
  private:
   std::vector<int> data_;
-  int count;
+  int count_;
 };
+
+void heap_sort(vector<int>& arr) {
+  Heap heap(std::move(arr));
+  arr.resize(0);
+  while (!heap.empty()) {
+    arr.emplace_back(heap.top());
+    heap.pop();
+    // print_result(arr, 0, -1, arr.size());
+  }
+}
 
 void test_heap_sort(int length, int method = 1) {
   vector<int> arr;
@@ -44,14 +115,14 @@ void test_heap_sort(int length, int method = 1) {
 
   auto start = std::chrono::steady_clock().now();
   if (method == 1) {
-    // bubble_sort(arr);
+    heap_sort(arr);
   } else if (method == 2) {
     // select_sort(arr);
   } else {
     // insert_sort(arr);
   }
   auto duration = std::chrono::steady_clock().now() - start;
-  printf("simple sort method %d total %d elements cost %lld.%03lld seconds\n",
+  printf("heap sort method %d total %d elements cost %lld.%03lld seconds\n",
          method, length, duration / chrono::seconds(1),
          duration % chrono::seconds(1) / chrono::milliseconds(1));
 
@@ -77,11 +148,11 @@ void print_result(vector<int>& arr, int begin, int pos, int end) {
   if (arr.size() > MAX_DISPLAY_SIZE) {
     return;
   }
-  static int count;
+  static int count_;
   if (pos == -1) {
-    count = 0;
+    count_ = 0;
   }
-  printf("%2d@ ", count);
+  printf("%2d@ ", count_);
   for (auto i = 0; i < arr.size(); i++) {
     if (i == begin) {
       std::cout << "( ";
@@ -96,7 +167,7 @@ void print_result(vector<int>& arr, int begin, int pos, int end) {
     }
   }
   printf("  #begin:%d pos:%d end:%d", begin, pos, end);
-  count++;
+  count_++;
   std::cout << endl;
 }
 
@@ -109,8 +180,6 @@ int main(int argc, char* argv[]) {
   auto size = std::stoi(string(para));
 
   test_heap_sort(size);
-  test_heap_sort(size, 2);
-  test_heap_sort(size, 3);
 
   return 0;
 }
