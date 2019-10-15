c7f33a833b2e03e9f81117d1b96e76f3a854bb6e
wangchenglin <wangcl.cw1@gmail.com>
2019-10-15 08:54:44 +0800

    update
    Signed-off-by: wangchenglin <wangcl.cw1@gmail.com>

--- 283.move-zeroes.cpp
+++ 283.move-zeroes.cpp
@@ -14,7 +14,7 @@ using namespace std;
 // @lc code=start
 class Solution {
  public:
-  void moveZeroes(vector<int>& nums) {
+  void moveZeroes2(vector<int>& nums) {
     int lastNonZero = -1;  // 指向非0元素最后一个位置
     for (size_t i = 0; i < nums.size(); i++) {
       /* code */
@@ -24,6 +24,19 @@ class Solution {
           std::swap(nums[i], nums[lastNonZero]);
         }
       }
+    }
+  }
+
+  void moveZeroes(vector<int>& nums) {
+    int count = 0;  // 非0元素个数
+    for (size_t i = 0; i < nums.size(); i++) {
+      /* code */
+      if (nums[i] != 0) {
+        nums[count++] = nums[i];
+      }
+    }
+    while (count < nums.size()) {
+      nums[count++] = 0;
     }
   }
 };
